ca265bf00099a58f67e3dff226e3691ca67a3633
muhammad-husnain-ali-web <mhusnainali913@gmail.com>
2025-10-14 13:24:31 +0500

    first.py

---
+++ first.py
@@ -0,0 +1,34 @@
+#This is single line command
+
+print("Hello World")
+
+print("My name is Husnain")
+print("I am 21 years old")
+print("Learnin python for AI")
+
+
+# This is a multi-line comment
+# It’s used for documentation or explanation
+
+"""
+This is a multi-line comment
+It’s used for documentation or explanation
+"""
+
+'''
+This is a multi-line comment
+It’s used for documentation or explanation
+'''
+
+# print('Name:\tHusnain\nAge:\t21')
+# print(f'Name:\tHusnain\nAge:\t21')
+
+print("""Name:    Husnain
+Age:     21""")
+
+print("Hello, I’m Husnain.\nI have experience in JavaScript.\nNow I’m learning Python for AI.")
+
+# This is same as
+print("""Hello, I’m Husnain.
+I have experience in JavaScript.
+Now I’m learning Python for AI.""")
